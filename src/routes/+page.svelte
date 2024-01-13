506414f399e7a10e100bcd6297b9d45c8be64683
ChoungPheng <45393928+Blizzard4E@users.noreply.github.com>
2024-01-14 02:26:54 +0700

    JS floating problems

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,16 +4,19 @@
         y1 = 0.492,
         y2 = -0.133;
     let key = "abcdefghijklmnop";
-    let plainText = "";
+    let plainText = "paragon";
+    let cipherValues = [];
+    let originalValues = [];
+    let decryptValues = [];
     let keyResults = [];
-    let y = [0, 0];
-    $: console.log(c1);
+
     let c1Prime, c2Prime;
     let keyError = false,
         plainTextError = false;
     let y1Prime = 0.242,
         y2Prime = -0.955;
     let cipherText = "";
+    let decryptedText = "";
     // Define the function f(x)
     function f(x) {
         return (x % 2) - 1;
@@ -24,6 +27,9 @@
     }
 
     function encrypt() {
+        decryptValues = [];
+        originalValues = [];
+        cipherValues = [];
         keyResults = [];
         cipherText = "";
         if (key.length < 16) {
@@ -37,6 +43,7 @@
         plainTextError = false;
         keyError = false;
 
+        let y = [0, 0];
         y[0] = y1;
         y[1] = y2;
         for (let i = 0; i < key.length; i++) {
@@ -52,16 +59,67 @@
         y[0] = y1Prime;
         y[1] = y2Prime;
         for (let i = 0; i < plainText.length; i++) {
-            //console.log(y[0] + ", " + y[1]);
-            let y_n = plainText.charCodeAt(i) + c1Prime * y[0] + c2Prime * y[1];
+            originalValues = [...originalValues, plainText.charCodeAt(i)];
+            let y_n = Math.round(
+                plainText.charCodeAt(i) + c1Prime * y[0] + c2Prime * y[1]
+            );
+            console.log(
+                plainText.charCodeAt(i) +
+                    " + " +
+                    c1Prime +
+                    " x " +
+                    y[0] +
+                    " + " +
+                    c2Prime +
+                    " x " +
+                    y[1] +
+                    " = " +
+                    y_n
+            );
+
+            cipherValues = [...cipherValues, y_n];
 
             y[1] = y[0];
             y[0] = y_n;
-            let boundedCharCode = y_n % 128; // Restrict to ASCII range 0-127
 
-            let encodedChar = String.fromCharCode(boundedCharCode);
+            let encodedChar = String.fromCharCode(y_n);
             cipherText += encodedChar;
         }
+        decrypt();
+    }
+
+    function decrypt() {
+        decryptedText = "";
+        let y = [0, 0];
+        y[0] = y1Prime;
+        y[1] = y2Prime;
+        console.log(" ");
+        for (let i = 0; i < cipherText.length; i++) {
+            let x_n = Math.round(
+                cipherText.charCodeAt(i) - c1Prime * y[0] - c2Prime * y[1]
+            );
+            console.log(
+                cipherText.charCodeAt(i) +
+                    " - " +
+                    c1Prime +
+                    " x " +
+                    y[0] +
+                    " - " +
+                    c2Prime +
+                    " x " +
+                    y[1] +
+                    " = " +
+                    x_n
+            );
+            decryptValues = [...decryptValues, x_n];
+
+            y[1] = y[0];
+            y[0] = x_n;
+
+            let encodedChar = String.fromCharCode(x_n);
+            decryptedText += encodedChar;
+        }
+        console.log(decryptedText);
     }
 </script>
 
@@ -189,9 +247,31 @@
         </section>
         <section class="w-full p-1">
             <h1 class="font-bold text-lg text-white">Main Algorithm</h1>
+            <div>
+                <h2 class="text-white font-bold">Original Values</h2>
+                {#each originalValues as value}
+                    <h3 class="text-white">{value}</h3>
+                {/each}
+            </div>
+            <div>
+                <h2 class="text-white font-bold">Cipher Values</h2>
+                {#each cipherValues as value}
+                    <h3 class="text-white">{value}</h3>
+                {/each}
+            </div>
+            <div>
+                <h2 class="text-white font-bold">Decrypt Values</h2>
+                {#each decryptValues as value}
+                    <h3 class="text-white">{value}</h3>
+                {/each}
+            </div>
             <h2 class="text-white">
-                <span class="font-bold">CipherText</span>
+                <span class="font-bold">Encrypted Text</span>
                 : {cipherText}
+            </h2>
+            <h2 class="text-white">
+                <span class="font-bold">Decrypted Text</span>
+                : {decryptedText}
             </h2>
         </section>
     </main>
