cf627b100c680ea1a969688f13cf0a08fd1c0e51
ChoungPheng <45393928+Blizzard4E@users.noreply.github.com>
2024-01-20 13:43:49 +0700

    Update +page.svelte

--- src/routes/phone/+page.svelte
+++ src/routes/phone/+page.svelte
@@ -2,6 +2,7 @@
     import Phone from "../../components/Phone.svelte";
 </script>
 
-<main class="w-full min-h-[100vh] bg-gray-900 text-white">
+<main class="w-full min-h-[100vh] bg-gray-900 text-white flex gap-8">
+    <div class="p-4"><Phone /></div>
     <div class="p-4"><Phone /></div>
 </main>
